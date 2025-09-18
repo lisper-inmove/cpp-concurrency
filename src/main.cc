8fa72b5e49a5379828fbc2f2df7e1cea2ba177a9
inmove <lisper.inmove@gmail.com>
2025-09-18 23:27:44 +0800

    future with packaged_task

--- src/main.cc
+++ src/main.cc
@@ -14,17 +14,30 @@ using namespace std;
 
  */
 
-int compute_square(int x) {
+double compute_square(int x, double y) {
     std::this_thread::sleep_for(std::chrono::seconds(2));
-    return x * x;
+    cout << "Compute Square thread " << std::this_thread::get_id() << endl;
+    cout << "Data is: " << x * x + y << endl;
+    return x * x * y + y;
 }
 
 int main() {
-    cout << "Starting async task ...\n";
-    std::future<int> result = std::async(std::launch::async, compute_square, 10);
-    cout << "Doing something else while waiting...\n";
+    cout << "Main Thread: " << std::this_thread::get_id() << endl;
 
-    int value = result.get();  // Waits until the computation finishes
-    cout << "Result: " << value << "\n";
+    // Future + async
+    // cout << "Starting async task ...\n";
+    // std::future<int> result = std::async(std::launch::async, compute_square, 10);
+    // cout << "Doing something else while waiting...\n";
+    // int value = result.get();  // Waits until the computation finishes
+    // cout << "Result: " << value << "\n";
+
+    // Future + packaged_task
+    std::packaged_task<double(int, double)> task(compute_square);
+    auto fut = task.get_future();
+    std::thread t(std::move(task), 10, 11.2);
+    cout << "Main is free to do other work...\n";
+    double result = fut.get();
+    std::cout << "Result: " << result << "\n";
+    t.join();
     return 0;
 }
