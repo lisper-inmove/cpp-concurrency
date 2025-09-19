2627d6d5218e3cc23a3f741ebed9ca51fb88ee7c
inmove <lisper.inmove@gmail.com>
2025-09-20 00:02:11 +0800

    example of shared_future

--- src/main.cc
+++ src/main.cc
@@ -7,28 +7,34 @@
 
 using namespace std;
 
-void worker(std::promise<int> p, bool fail) {
-    try {
-        if (fail) {
-            throw std::runtime_error("something went wrong");
-        }
-        p.set_value(42);
-    } catch (...) {
-        // p.set_exception(std::current_exception());
-        p.set_exception(std::make_exception_ptr(std::logic_error("An Error")));
-    }
+int compute_value() {
+    cout << "Computing value ... \n";
+    std::this_thread::sleep_for(std::chrono::seconds(2));
+    return 42;
 }
 
 int main() {
-    std::promise<int> p;
-    auto f = p.get_future();
-    std::thread t(worker, std::move(p), true);
+    std::packaged_task<int()> task(compute_value);
+    std::future<int> fut = task.get_future();
+    auto shared_fut = fut.share();
+    // std::shared_future<int> shared_fut = fut.share();
+    // std::shared_future<int> shared_fut(std::move(fut)); // 也可以用move
+    std::thread t(std::move(task));
+
+    std::thread consumer1([shared_fut] {
+        cout << "Consumer 1 waiting ...\n";
+        int value = shared_fut.get();
+        cout << "Consumer 1 got value: " << value << "\n";
+    });
+
+    std::thread consumer2([shared_fut] {
+        cout << "Consumer 2 waiting ...\n";
+        int value = shared_fut.get();
+        cout << "Consumer 2 got value: " << value << "\n";
+    });
+
+    consumer1.join();
+    consumer2.join();
     t.join();
-    try {
-        int result = f.get();
-        cout << "Got result: " << result << "\n";
-    } catch (const std::exception &e) {
-        cout << "Caught exception: " << e.what() << "\n";
-    }
     return 0;
 }
