3e56c99ceb82ad012a4f055032e04800756b7c1e
inmove <lisper.inmove@gmail.com>
2025-09-19 19:53:21 +0800

    throw an error when using async and future

--- src/main.cc
+++ src/main.cc
@@ -1,30 +1,20 @@
 #include <iostream>
 #include <future>
+#include <stdexcept>
 #include <thread>
 #include <chrono>
 
 using namespace std;
 
-void worker(std::promise<int> prom) {
-    std::this_thread::sleep_for(std::chrono::seconds(2));
-    std::cout << "Worker thread calculating..." << std::this_thread::get_id() << "\n";
-    // prom.set_value(42);  // set result
-    // raise an error
+int worker(int x) {
+    if (x < 0) {
+        throw std::out_of_range("x < 0");
+    }
+    return x * x;
 }
 
 int main() {
-    std::promise<int> prom;
-    std::future<int> fut = prom.get_future();
-
-    std::thread t(worker, std::move(prom));
-
-    std::cout << "Main thread waiting..." << std::this_thread::get_id() << "\n";
-    try {
-        int value = fut.get();  // blocks until worker sets the value
-        std::cout << "Got value: " << value << "\n";
-    } catch (const std::future_error &e) {
-        cout << "Prom destroyed" << endl;
-    }
-    t.join();
+    auto f = std::async(std::launch::async, worker, 1);
+    cout << "result is: " << f.get() << endl;
     return 0;
 }
