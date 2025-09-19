d2a6ea98c18bfb7899473062f1b0dfcb2a6d18b4
inmove <lisper.inmove@gmail.com>
2025-09-19 19:49:22 +0800

    throw an exception when no set for promise

--- src/main.cc
+++ src/main.cc
@@ -5,39 +5,26 @@
 
 using namespace std;
 
-/**
-
-   std::async
-   policy: 默认值为 std::launch::async | std::launch::deferred，由实现自行决定
-       std::launch::async: 新线程里立即执行
-       std::launch::deferred: 直到future.get 或 future.wait时才执行
-
- */
-
-double compute_square(int x, double y) {
+void worker(std::promise<int> prom) {
     std::this_thread::sleep_for(std::chrono::seconds(2));
-    cout << "Compute Square thread " << std::this_thread::get_id() << endl;
-    cout << "Data is: " << x * x + y << endl;
-    return x * x * y + y;
+    std::cout << "Worker thread calculating..." << std::this_thread::get_id() << "\n";
+    // prom.set_value(42);  // set result
+    // raise an error
 }
 
 int main() {
-    cout << "Main Thread: " << std::this_thread::get_id() << endl;
+    std::promise<int> prom;
+    std::future<int> fut = prom.get_future();
 
-    // Future + async
-    // cout << "Starting async task ...\n";
-    // std::future<int> result = std::async(std::launch::async, compute_square, 10);
-    // cout << "Doing something else while waiting...\n";
-    // int value = result.get();  // Waits until the computation finishes
-    // cout << "Result: " << value << "\n";
+    std::thread t(worker, std::move(prom));
 
-    // Future + packaged_task
-    std::packaged_task<double(int, double)> task(compute_square);
-    auto fut = task.get_future();
-    std::thread t(std::move(task), 10, 11.2);
-    cout << "Main is free to do other work...\n";
-    double result = fut.get();
-    std::cout << "Result: " << result << "\n";
+    std::cout << "Main thread waiting..." << std::this_thread::get_id() << "\n";
+    try {
+        int value = fut.get();  // blocks until worker sets the value
+        std::cout << "Got value: " << value << "\n";
+    } catch (const std::future_error &e) {
+        cout << "Prom destroyed" << endl;
+    }
     t.join();
     return 0;
 }
