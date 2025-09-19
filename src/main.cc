41f7fc855611834967e773390e28ca496b5c4453
inmove <lisper.inmove@gmail.com>
2025-09-19 22:33:49 +0800

    throw exception use promise for future

--- src/main.cc
+++ src/main.cc
@@ -1,3 +1,4 @@
+#include <exception>
 #include <iostream>
 #include <future>
 #include <stdexcept>
@@ -6,15 +7,28 @@
 
 using namespace std;
 
-int worker(int x) {
-    if (x < 0) {
-        throw std::out_of_range("x < 0");
+void worker(std::promise<int> p, bool fail) {
+    try {
+        if (fail) {
+            throw std::runtime_error("something went wrong");
+        }
+        p.set_value(42);
+    } catch (...) {
+        // p.set_exception(std::current_exception());
+        p.set_exception(std::make_exception_ptr(std::logic_error("An Error")));
     }
-    return x * x;
 }
 
 int main() {
-    auto f = std::async(std::launch::async, worker, 1);
-    cout << "result is: " << f.get() << endl;
+    std::promise<int> p;
+    auto f = p.get_future();
+    std::thread t(worker, std::move(p), true);
+    t.join();
+    try {
+        int result = f.get();
+        cout << "Got result: " << result << "\n";
+    } catch (const std::exception &e) {
+        cout << "Caught exception: " << e.what() << "\n";
+    }
     return 0;
 }
