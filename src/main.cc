f37b1b009b1bc513dd697ededdb3dfea57370f51
inmove <lisper.inmove@gmail.com>
2025-09-24 22:56:14 +0800

    example of atomic_flag (implement spin-lock)

--- src/main.cc
+++ src/main.cc
@@ -1,45 +1,30 @@
 #include <exception>
-#include <iostream>
-#include <future>
-#include <stdexcept>
 #include <thread>
+#include <latch>
+#include <iostream>
+#include <atomic>
 #include <chrono>
-
-#include "quick_sort.h"
 
 using namespace std;
 
-template <typename T>
-void print_list(const std::list<T> &lst, const std::string& name) {
-    cout << name << ": ";
-    for (auto const& val : lst) {
-        cout << val << " ";
-    }
-    cout << "\n";
+atomic_flag lock= ATOMIC_FLAG_INIT;
+
+void critical_section(int id) {
+
+    // 这就相当于一个自旋锁
+    while(lock.test_and_set(std::memory_order_acquire)) {}
+
+    cout << "Thread " << id << " entering critical section\n";
+    std::this_thread::sleep_for(std::chrono::seconds(1));
+    cout << "Thread " << id << " leaving critical section\n";
+    lock.clear(std::memory_order_release);
 }
 
 int main() {
-    std::list<int> data;
-    for (int i = 0; i < 2000; i++) {
-        data.push_back(rand() % 100000);
-    }
+    std::thread t1(critical_section, 1);
+    std::thread t2(critical_section, 2);
 
-    // cout << "Original: ";
-    // for (auto v: data) cout << v << " ";
-    // cout << "\n";
-
-    auto start_seq = std::chrono::high_resolution_clock::now();
-    auto sorted_seq = sequential_quick_sort(data);
-    auto end_seq = std::chrono::high_resolution_clock::now();
-    auto dur_seq = std::chrono::duration_cast<std::chrono::milliseconds>(end_seq - start_seq).count();
-    cout << "Sequential Time Consum: " << dur_seq << "\n";
-    // print_list(sorted_seq, "Sequential Quick Sort");
-
-    auto start_par = std::chrono::high_resolution_clock::now();
-    auto sorted_par = parallel_quick_sort(data);
-    auto end_par = std::chrono::high_resolution_clock::now();
-    auto dur_par = std::chrono::duration_cast<std::chrono::milliseconds>(end_par - start_par).count();
-    cout << "Parallel Time Consum: " << dur_par << "\n";
-    // print_list(sorted_par, "Parallel Quick Sort");
+    t1.join();
+    t2.join();
     return 0;
 }
